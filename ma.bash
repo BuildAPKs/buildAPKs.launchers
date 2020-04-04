dcf8c3bf4afbe14087169722d27765402813db3d
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-04-04 14:28:25 -0400

    commit 201949995 on 20200404

--- ma.bash
+++ ma.bash
@@ -9,4 +9,5 @@ cd "$JDR"
 # _AT_ username/repository commit
 _AT_ amsitlab/termuxlauncher b543e1d0e20cb36ce0c801ad3cbee0d27c055882
 _AT_ powerpoint45/BasicLauncher 55493d60d1addc29330dd9f42f8cfb547578e28f
+_AT_ godsmoon/Moto-Home-Fix d1231bf6adf4d2cccd38dc726e2c7374caa2d76b
 # ma.bash OEF
