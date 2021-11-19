4831eb9b28bc1dd075cd112a7e88d7e65594e2c2
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-11-19 00:21:40 -0500

    commit 667058581 on 20211119

--- ma.bash
+++ ma.bash
@@ -8,6 +8,7 @@ shopt -s nullglob globstar
 cd "$JDR"
 # _AT_ username/repository commit
 _AT_ amsitlab/termuxlauncher b543e1d0e20cb36ce0c801ad3cbee0d27c055882
+_AT_ chelovek84/mLauncher ee609c619650ce10ae6d0eaf90bd4a9d050aac0c
 _AT_ powerpoint45/BasicLauncher 55493d60d1addc29330dd9f42f8cfb547578e28f
 _AT_ godsmoon/Moto-Home-Fix d1231bf6adf4d2cccd38dc726e2c7374caa2d76b
 # ma.bash OEF
