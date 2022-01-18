f881b15a44b6c2474074d5f97e059ac1967f8d1e
SDRausty <27742457+SDRausty@users.noreply.github.com>
2022-01-18 16:38:00 -0500

    commit 974277432 on 20220118

--- ma.bash
+++ ma.bash
@@ -1,5 +1,5 @@
-#!/usr/bin/env bash 
-# Copyright 2019-2020 (c) all rights reserved by BuildAPKs; see LICENSE 
+#!/usr/bin/env bash
+# Copyright 2019-2020 (c) all rights reserved by BuildAPKs; see LICENSE
 # https://buildapks.github.io
 ################################################################################
 set -Eeuo pipefail
@@ -7,7 +7,8 @@ shopt -s nullglob globstar
 . "$RDR"/scripts/bash/shlibs/trap.bash 210 211 212
 cd "$JDR"
 # _AT_ username/repository commit
-_AT_ amsitlab/termuxlauncher b543e1d0e20cb36ce0c801ad3cbee0d27c055882
+_AT_ amsitlab/termuxlauncher a076d03b9bccea427ffce2c122444186e2f49656
+# _AT_ amsitlab/termuxlauncher b543e1d0e20cb36ce0c801ad3cbee0d27c055882
 _AT_ chelovek84/mLauncher ee609c619650ce10ae6d0eaf90bd4a9d050aac0c
 _AT_ powerpoint45/BasicLauncher 55493d60d1addc29330dd9f42f8cfb547578e28f
 _AT_ godsmoon/Moto-Home-Fix d1231bf6adf4d2cccd38dc726e2c7374caa2d76b
